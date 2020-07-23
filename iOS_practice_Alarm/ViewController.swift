5a1f3b2a7010aa98e7f6dea1583f21ea902825ec
jerry960331 <jerry960331@gmail.com>
2020-07-23 15:36:45 +0800

    Initial Commit

---
+++ iOS_practice_Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_practice_Alarm
+//
+//  Created by 歐東 on 2020/7/23.
+//  Copyright © 2020 歐東. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
